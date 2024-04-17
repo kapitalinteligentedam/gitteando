fb150545074a07eb70d94c0e3ff1225c9f850ed2
kapitalinteligentedam <carlos.perez@kapitalinteligente.com>
2024-04-17 15:18:57 +0200

    primer commit

---
+++ bin/gitteando.dart
@@ -0,0 +1,5 @@
+import 'package:gitteando/gitteando.dart' as gitteando;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gitteando.calculate()}!');
+}
---
+++ test/gitteando_test.dart
@@ -0,0 +1,8 @@
+import 'package:gitteando/gitteando.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
